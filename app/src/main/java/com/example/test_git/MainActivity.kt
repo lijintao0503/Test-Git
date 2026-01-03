f90f108693fd41e1b1cd30f13dfc40ceddd6497f
lijintao <lijintao@drojian.degv>
2026-01-03 15:52:46 +0800

    feat master: update MainActivity

--- app/src/main/java/com/example/test_git/MainActivity.kt
+++ app/src/main/java/com/example/test_git/MainActivity.kt
@@ -17,6 +17,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Log.e("git", "onCreate:  dev" )
+        Log.e("git", "onCreate:  master" )
     }
 }
